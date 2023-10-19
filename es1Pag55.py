6f8443d6dc9b7f9d085eda0413d72e9135661117
Michele Pellegrino <michele.pellegrino.133@itiscuneo.eu>
2023-10-19 19:16:00 +0200

    es 1 pag. 55

---
+++ es1Pag55.py
@@ -0,0 +1,13 @@
+def main():
+    s = "ciao"
+    b = True
+    i = 5
+    f = 0.3
+    
+    print(f"{s} --> {type(s)}")
+    print(f"{b} --> {type(b)}")
+    print(f"{i} --> {type(i)}")
+    print(f"{f} --> {type(f)}")
+
+if __name__ == "__main__":
+    main()

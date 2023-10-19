3d4d5e4c3d0dcaadf82f3d0bca2eb917e32ac919
Michele Pellegrino <michele.pellegrino.133@itiscuneo.eu>
2023-10-19 19:11:45 +0200

    es sulla turtle

---
+++ es6Logo.py
@@ -0,0 +1,9 @@
+import turtle #esempio di impostazione di un modulo built-in (nativo)
+
+finestra = turtle.Screen()
+sciolla = turtle.Turtle()
+sciolla.forward(100)
+sciolla.left(50)
+sciolla.forward(100)
+
+finestra.mainloop()#gestione finestra
